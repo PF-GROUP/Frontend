ff2274b64196f436d18e698114cadf679e2ad68f
Joaco.M <joaco.martinez1480@gmail.com>
2025-06-25 11:29:16 -0300

    pagina de maquetacion chatbot

---
+++ src/app/(view)/chatbotPrueba/page.tsx
@@ -0,0 +1,19 @@
+const ChatbotPrueba = () => {
+  return (
+    <div className="flex justify-center mt-10 w-screen h-screen border-4 border-black-300">
+        
+        
+        
+        {/* aca podes poner el script del chatbot */}
+
+
+
+    </div>
+  );
+};
+
+
+// inicia el proyecto con npm run dev
+// para ver esta pagina pone localhost:{puerto}/chatbotPrueba 
+// suerte amigazo jaja
+export default ChatbotPrueba
