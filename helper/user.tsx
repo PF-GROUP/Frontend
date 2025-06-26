47f37489cb5db048c5f6f7a55d8c59bd9d8226bd
Joaco.M <joaco.martinez1480@gmail.com>
2025-06-26 01:53:33 -0300

    aaa

--- helper/user.tsx
+++ helper/user.tsx
@@ -1,6 +1,16 @@
 // import { IUser } from "../interface/User";
+// import { IUser } from "../interface/User";
 
 
+// export const adminUser: IUser = {
+//   id: 1,
+//   name: "Juan",
+//   surname: "Salvo",
+//   email: "admin@kasapp.com",
+//   phone: "+54 911 1234 5678",
+//   password: "admin123",
+//   isAdmin: true,
+// };
 // export const adminUser: IUser = {
 //   id: 1,
 //   name: "Juan",
@@ -15,12 +25,21 @@
 
 
 
-export const agenteUser: IUser = {
-  id: 1,
-  name: "Juan",
-  surname: "Salvo",
-  email: "admin@kasapp.com",
-  phone: "+54 911 1234 5678",
-  password: "admin123",
-  isAdmin: false,
-};
+// export const agenteUser: IUser = {
+//   id: 1,
+//   name: "Juan",
+//   surname: "Salvo",
+//   email: "admin@kasapp.com",
+//   phone: "+54 911 1234 5678",
+//   password: "admin123",
+//   rol: 1,
+// };
+// export const agenteUser: IUser = {
+//   id: 1,
+//   name: "Juan",
+//   surname: "Salvo",
+//   email: "admin@kasapp.com",
+//   phone: "+54 911 1234 5678",
+//   password: "admin123",
+//   isAdmin: false,
+// };
